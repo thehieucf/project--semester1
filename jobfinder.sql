06ae06ad5366d84dcea827f2f843938db34318d7
hieupt <hieu.ptt.2586@aptechlearning.edu.vn>
2025-09-10 18:35:33 +0700

    sql

---
+++ jobfinder.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE jobfinder;
+USE jobfinder;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
